4b0b0c3b20e7aca5ea7022d5211318d4f24e0d0c
Blaine <42214598+Blakeinstein@users.noreply.github.com>
2018-10-04 11:36:09 +0530

    Create ram.c
    Hardy-Ramanujan Numbers

---
+++ ram.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a=0,b,c,d,e;
+    for(e=1;a<5;e++)
+    {
+      b=0;
+       for (c=1; c<=pow(e,1.0/3); c++)
+       {
+          for(d=c+1; d<=pow(e,1.0/3); d++)
+          {
+              if(c*c*c+d*d*d == e)
+              b++;
+          }
+       }
+       if(b == 2)
+       {
+          a++;
+          printf("%d \n",e);
+       }
+    }
+}
