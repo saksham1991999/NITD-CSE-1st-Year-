96729f2ab1948711d63ba4b8ec9d1f1fffa5206c
Blaine <42214598+Blakeinstein@users.noreply.github.com>
2018-10-05 13:43:18 +0530

    Create equfun.c
    Using functions to compare two numbers

---
+++ equfun.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int compare(int a,int b)
+{
+  int c;
+  if (a==b)
+  c=0;
+  else
+  c=(a>b? a:b);
+  return c;
+}
+void main()
+{
+  int a,b;
+  scanf("%d %d",&a,&b);
+  if (compare(a,b)=0)
+  printf("equal");
+  else
+  printf("%d",compare(a,b));
+}  
+  
