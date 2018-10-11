f529bb513e7d2f57fe634edbfad3a3294b155228
Blaine <42214598+Blakeinstein@users.noreply.github.com>
2018-10-11 15:24:36 +0530

    Create fibofn.c

---
+++ fibofn.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int b=0,a=0;
+int fib(int c,int t)
+{
+  if(t==0)
+  return;
+  else
+  {
+    printf("%d ",c);
+    a=b;
+    b=c;
+    t--;
+    return fib(a+b,t);
+  }
+  printf(" \n");
+}
+int main()
+{
+  int t;
+  printf("How many terms? ");
+  scanf("%d",&t);
+  fib(1,t+1);
+}
