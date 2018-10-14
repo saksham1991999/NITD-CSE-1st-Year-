33c23fd7a760f6170308fbd6f009fffd3cad5c21
Blaine <Rishi1998@gmail.com>
2018-10-14 05:37:16 +0530

    Update sorarmer.c

--- sorarmer.c
+++ sorarmer.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-  int a[100],b[100],c[100],d,e,i,j,k;
+  int a[100],b[100],c[100],d,e,i,j,k=0;
   printf("Describe array 1 and 2.\n Their sizes are? ");
   scanf("%d %d",&d,&e);
   printf("input values of first array \n");
@@ -19,29 +19,18 @@ int main()
     c[i]=a[i];
     c[(d>e?e:d)+i]=b[i];
   }
-  for(i=0,k=c[0],j=0;j!=d+e-1;)
+  for(i=0;i<d+e;i++)
   {
-    if(c[i]<k&&i<d+e-1)
+    for(j=0;j<(d+e-i-1);j++)
     {
-      k=c[i];
-      i=0;
-      j++;
+        if(c[j]>c[j + 1])
+        {
+            k=c[j];
+            c[j]=c[j + 1];
+            c[j + 1]=k;
+        }
     }
-    else
-    i++;
-  }
-  for(i=0,j=0;j!=d+e-1;)
-  {
-    if(c[i]>k&&i<d+e-1)
-    {
-      k=c[i];
-      a[j]=c[i];
-      j++;
-      i=0;
-    }
-    else
-    i++;
-  }
+}
   printf("the merged array\n");
   for(i=0;i<d+e;i++)
   {
